09f5da180a484e157671a894bb931796fdc4de66
rafaluca784 <142506655+rafaluca784@users.noreply.github.com>
2024-03-18 11:48:03 -0300

    Add files via upload

---
+++ tema6/desafios/get/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Bem-vindo</title>
+</head>
+<body>
+    <?php
+    if(isset($_GET['nome'])) {
+
+	//código aqui
+
+    $nome = $_GET['nome'];
+    echo " bem vindo ". $nome;
+
+    }
+    ?>
+</body>
+</html>
